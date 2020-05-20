8c6599b004f27bc7df9645d02db77221d94b6d6e
Cezary-Pokropek <cezary.pokropek@gmail.com>
2020-05-20 12:25:05 +0200

    Setup Java class for JSON parsing

---
+++ EarthQuakeAppAndroidJava/app/src/main/java/cezary/pokropek/earthquake/Util/Constants.java
@@ -0,0 +1,9 @@
+package cezary.pokropek.earthquake.Util;
+
+public class Constants {
+
+    public static final String URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_week.geojson";
+    private static final int LIMIT = 30;
+
+
+}
